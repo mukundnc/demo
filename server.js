a83978561efe0ebb7e17134b4c1ed91904f17a37
mukundnc <mukund.nc@gmail.com>
2016-01-14 18:00:04 +0530

    fixed an issue

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = new express();
+
+app.use(express.static('app'));
+
+app.use(function(err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).json({success:false, data:'internal server error'});
+});
+
+var port = 1111;
+app.listen(port);
+console.log('server running at port - ' + port);
